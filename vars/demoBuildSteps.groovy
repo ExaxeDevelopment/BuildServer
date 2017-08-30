5ae049d7bb6e79768bf2d33e2c29af83ecb9cde0
FilipLudma <31214892+FilipLudma@users.noreply.github.com>
2017-08-30 14:11:03 +0100

    - Updated Directories

---
+++ vars/demoBuildSteps.groovy
@@ -0,0 +1,31 @@
+class buildSteps implements Serializable {
+    def createBuildSteps(val) { 
+        List<String> list = new ArrayList<String>();
+        switch (val) {
+            case "Update":
+                list.add("Stage2");
+                break
+            case "Deploy":
+                list.add("Stage3");
+                break
+            default:
+                list.add("GetAllCodeLatestVersion");
+                list.add("GetCodeLatestVersion");
+                list.add("ClearDatabases");
+                list.add("InstallAllDatabases");
+                list.add("CheckOutFiles");
+                list.add("UpdateAssemblyVersion");
+                list.add("RestoreNuGetPackages");
+                list.add("UpdateNuGetPackages");
+                list.add("BuildSolution");
+                list.add("PublishWebService");
+                list.add("CheckWorkflowsAsync");
+                list.add("PerformUnitTests");
+                list.add("CheckInFiles");
+                list.add("PublishNuGetPackages");
+                list.add("CheckNuGetFeed");
+                break
+        }   
+        list
+    }
+}
