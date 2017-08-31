311d29a068f8750514be84f59c7b9121d1014a13
FilipLudma <31214892+FilipLudma@users.noreply.github.com>
2017-08-31 13:46:34 +0100

    - Updated mapped objects;

--- vars/deployCommonBuildSteps.groovy
+++ vars/deployCommonBuildSteps.groovy
@@ -3,16 +3,20 @@ class deployCommonBuildSteps implements Serializable {
 
 		def emptyMap = [:];
 		Map<String,String> deployMap1  = new HashMap<String,String>();
-		deployMap1.put("Initialisation", "Initialisation"); 
+		deployMap1.put("Project", "Initialisation"); 
+		deployMap1.put("Operation", "Initialisation"); 
 		Map<String,String> deployMap2  = new HashMap<String,String>();
-		deployMap2.put("GetAllCodeLatestVersion", "GetAllCodeLatestVersion"); 
+		deployMap2.put("Project", "GetAllCodeLatestVersion"); 
+		deployMap2.put("Operation", "GetAllCodeLatestVersion"); 
 		Map<String,String> deployMap3  = new HashMap<String,String>();
-		deployMap3.put("Exaxe.SolutionManager", "GetCodeLatestVersion"); 
+		deployMap3.put("Project", "Exaxe.SolutionManager"); 
+		deployMap3.put("Operation", "GetCodeLatestVersion"); 
 		Map<String,String> deployMap4  = new HashMap<String,String>();
-		deployMap4.put("Exaxe.SolutionManager", "RestoreNuGetPackages"); 
+		deployMap4.put("Project", "Exaxe.SolutionManager"); 
+		deployMap4.put("Operation", "RestoreNuGetPackages"); 
 		Map<String,String> deployMap5  = new HashMap<String,String>();
-		deployMap5.put("Exaxe.SolutionManager", "BuildSolution"); 
-
+		deployMap5.put("Project", "Exaxe.SolutionManager"); 
+		deployMap5.put("Operation", "BuildSolution"); 
 
 		List l1 = ["Initialisation", "Initialisation"]
 		List l2 = ["GetAllCodeLatestVersion", "GetAllCodeLatestVersion"]
