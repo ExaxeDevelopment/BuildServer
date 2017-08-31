f87017fbae9646450bafc4a43b259589692d8f74
FilipLudma <31214892+FilipLudma@users.noreply.github.com>
2017-08-31 12:34:50 +0100

    - Updated the buildStepsByBuildType.groovy file. Updated initialization of ArrayLists;

--- vars/buildStepsByBuildType.groovy
+++ vars/buildStepsByBuildType.groovy
@@ -2,19 +2,19 @@ class buildStepsByBuildType implements Serializable {
     def createBuildStepsByBuildType(val) { 
 
         List<String> fullBuild = 
-		new ArrayList<String>{{add("GetAllCodeLatestVersion"), add("ClearDatabases"), add("InstallAllDatabases"), add("CheckOutFiles"), add("UpdateAssemblyVersion"), add("RestoreNuGetPackages"), add("UpdateNuGetPackages"), add("BuildSolution"), add("PerformUnitTests"), add("CheckInFiles"), add("PublishNuGetPackages"), add("CheckNuGetFeed"), add("DistributeSQLScripts")}};
+		new ArrayList<String>{{add("GetAllCodeLatestVersion"); add("ClearDatabases"); add("InstallAllDatabases"); add("CheckOutFiles"); add("UpdateAssemblyVersion"); add("RestoreNuGetPackages"); add("UpdateNuGetPackages"); add("BuildSolution"); add("PerformUnitTests"); add("CheckInFiles"); add("PublishNuGetPackages"); add("CheckNuGetFeed"); add("DistributeSQLScripts")}};
 
         List<String> fullBuildWithoutGetAllCodeLatestVersionStage = 
-		new ArrayList<String>{{ add("GetAllCodeLatestVersion"), add("ClearDatabases"), add("InstallAllDatabases"), add("CheckOutFiles"), add("UpdateAssemblyVersion"), add("RestoreNuGetPackages"), add("UpdateNuGetPackages"), add("BuildSolution"), add("PerformUnitTests"), add("CheckInFiles"), add("PublishNuGetPackages"), add("CheckNuGetFeed"), add("DistributeSQLScripts")}};
+		new ArrayList<String>{{ add("GetAllCodeLatestVersion"); add("ClearDatabases"); add("InstallAllDatabases"); add("CheckOutFiles"); add("UpdateAssemblyVersion"); add("RestoreNuGetPackages"); add("UpdateNuGetPackages"); add("BuildSolution"); add("PerformUnitTests"); add("CheckInFiles"); add("PublishNuGetPackages"); add("CheckNuGetFeed"); add("DistributeSQLScripts")}};
 		
 		List<String> buildWithoutDB = 
-		new ArrayList<String>{{add("GetCodeLatestVersion"), add("CheckOutFiles"), add("UpdateAssemblyVersion"), add("RestoreNuGetPackages"), add("UpdateNuGetPackages"), add("BuildSolution"), add("PerformUnitTests"), add("CheckInFiles"), add("PublishNuGetPackages"), add("CheckNuGetFeed")}};
+		new ArrayList<String>{{add("GetCodeLatestVersion"); add("CheckOutFiles"); add("UpdateAssemblyVersion"); add("RestoreNuGetPackages"); add("UpdateNuGetPackages"); add("BuildSolution"); add("PerformUnitTests"); add("CheckInFiles"); add("PublishNuGetPackages"); add("CheckNuGetFeed")}};
 		
 		List<String> buildUI = 
-		new ArrayList<String>{{add("GetCodeLatestVersion"), add("CheckOutFiles"), add("UpdateAssemblyVersion"), add("RestoreNuGetPackages"), add("UpdateNuGetPackages"), add("BuildSolution"), add("PerformUnitTests"), add("CheckInFiles")}};
+		new ArrayList<String>{{add("GetCodeLatestVersion"); add("CheckOutFiles"); add("UpdateAssemblyVersion"); add("RestoreNuGetPackages"); add("UpdateNuGetPackages"); add("BuildSolution"); add("PerformUnitTests"); add("CheckInFiles")}};
 
 		List<String> buildDBSyncService =
-		new ArrayList<String>{{add("GetAllCodeLatestVersion"), add("GetCodeLatestVersion"), add("CheckOutFiles"), add("RestoreNuGetPackages") ,add("BuildSolution"), add("PerformUnitTests"), add("Provision"), add("Synchronize"), add("InstallAllDatabases"), add("Reprovision"), add("Synchronize")}}
+		new ArrayList<String>{{add("GetAllCodeLatestVersion"); add("GetCodeLatestVersion"); add("CheckOutFiles"); add("RestoreNuGetPackages") ;add("BuildSolution"); add("PerformUnitTests"); add("Provision"); add("Synchronize"); add("InstallAllDatabases"); add("Reprovision"); add("Synchronize")}};
 		
 		List<String> buildSteps = new ArrayList<String>();
         switch (val) {
