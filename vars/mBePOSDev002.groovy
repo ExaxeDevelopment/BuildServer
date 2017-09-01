c90a9500112fb8d76a3f452a8dfa16b5978d6146
FilipLudma <31214892+FilipLudma@users.noreply.github.com>
2017-09-01 12:38:22 +0100

    - Added mBePOSDev002 - Returns MasterBuild Stages for ePos dev002;

---
+++ vars/mBePOSDev002.groovy
@@ -0,0 +1,23 @@
+class mBePOSDev002 implements Serializable {
+    def getMasterBuildSteps(val) { 
+        
+		List<String> masterBuildSteps = new ArrayList<String>();
+
+		List<String> allSteps = new ArrayList<String>();
+        list.add("Build-Exaxe.Client-Dev003");
+        list.add("Build-Exaxe.FactFind-Dev002");
+        list.add("Build-Exaxe.Quote.Rules-Dev001");
+        list.add("Build-Exaxe.Quote-Dev002");
+        list.add("Build-Exaxe.ExistingBusiness-Dev001");
+        list.add("Build-PointOfSaleUI-Dev001");
+        list.add("Deploy-ePOS-Dev002-ACORN");
+		
+		Int indexOfStartStep = allSteps.findIndexOf { name -> name == val };
+        
+		masterBuildSteps = 
+			allSteps.subList(Math.max(allSteps.size() - indexOfStartStep, 0), allSteps.size());
+		
+		// Return Selected MasterBuildSteps
+		masterBuildSteps
+    }
+}
