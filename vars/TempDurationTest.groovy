21aa15e16872fd6386077421dd5d5a4c30b29d8f
FilipLudma <31214892+FilipLudma@users.noreply.github.com>
2017-09-07 16:02:23 +0100

    - Added TempDurationTest Jenkins file - added to test build time measurement;

---
+++ vars/TempDurationTest.groovy
@@ -0,0 +1,28 @@
+import hudson.Util;
+
+node{
+    withCredentials([[$class: "UsernamePasswordMultiBinding", credentialsId: "166ca05f-1074-4a9c-9529-2ab17ba62480", usernameVariable: "USERNAME", passwordVariable: "PASSWORD"]]) {
+        tfsUsername = "${env.USERNAME}"
+        tfsPassword = "${env.PASSWORD}"
+    }
+
+    def deployStepsClass = new deployCommonBuildSteps();
+    def deployCommonSteps = deployStepsClass.getDeployCommonBuildSteps("DeployEposPilotACORN")
+    
+    for(Map<String,String>step : deployCommonSteps){
+        if(step.get("Project") == step.get("Operation")){
+            stage(step.get("Operation")){
+            
+            }
+        } 
+        else{
+            def stageName = "${step.get("Project")} - ${step.get("Operation")}"
+            stage("${stageName}"){
+            
+            }
+        }
+    }
+    
+    duration = "Build duration: ${Util.getTimeSpanString(System.currentTimeMillis() - currentBuild.startTimeInMillis)}";
+    echo duration.toString();
+}
