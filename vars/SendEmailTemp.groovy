6acbd584cb8a3acd4d83fdea6e73ce9a2dd045dd
ExaxeDevelopment <build.development@exaxe.com>
2017-10-12 17:42:47 +0100

    - Updated send email temp;

--- vars/SendEmailTemp.groovy
+++ vars/SendEmailTemp.groovy
@@ -12,47 +12,51 @@ node{
 		selectedJiraRelease = SelectedJiraRelease;
 		selectedJiraProjectKey = SelectedJiraProjectKey; 
 
-		def jiraStatuses = "";
-		switch (selectedJiraProjectKey) {
-			case "OF":
-                jiraStatuses = "status in ('Dev Complete', 'Test In Progress')";
-				selectedJiraProjectKey = "'OF'";
-                break
-			default:
-				jiraStatuses = "status = Resolved";
-				break
-		}
+		def body = "${env.BUILD_URL}"; 
 
-		def jql = "project = " + selectedJiraProjectKey + " AND " + jiraStatuses;
-		def issues = jiraJqlSearch jql: jql, site: 'exaxejira', failOnError: true;
+		if(selectedJiraProjectKey != null || selectedJiraProjectKey != ""){
+			def jiraStatuses = "";
+			switch (selectedJiraProjectKey) {
+				case "OF":
+					jiraStatuses = "status in ('Dev Complete', 'Test In Progress')";
+					selectedJiraProjectKey = "'OF'";
+					break
+				default:
+					jiraStatuses = "status = Resolved";
+					break
+			}
+
+			def jql = "project = " + selectedJiraProjectKey + " AND " + jiraStatuses;
+			def issues = jiraJqlSearch jql: jql, site: 'exaxejira', failOnError: true;
 		
-		def body = "${env.BUILD_URL}" + "\r\n\r\n" + "JIRA RELEASE NOTES" + "\r\n\r\n";
-		def jiraUrl = "https://exaxejira.atlassian.net/browse/"
+			body = "${env.BUILD_URL}" + "\r\n\r\n" + "JIRA RELEASE NOTES" + "\r\n\r\n";
+			def jiraUrl = "https://exaxejira.atlassian.net/browse/"
 
-		for(def issue in issues.data.issues){
-			def tempJiraSummary = "";   
-			tempJiraSummary = "Summary: ";
-			tempJiraSummary = tempJiraSummary + issue.key + ": " + issue.fields.summary + endRow;
+			for(def issue in issues.data.issues){
+				def tempJiraSummary = "";   
+				tempJiraSummary = "Summary: ";
+				tempJiraSummary = tempJiraSummary + issue.key + ": " + issue.fields.summary + endRow;
 
-			tempJiraSummary = tempJiraSummary + "Status: ";
-			tempJiraSummary = tempJiraSummary +  issue.fields.status.name + endRow;			
+				tempJiraSummary = tempJiraSummary + "Status: ";
+				tempJiraSummary = tempJiraSummary +  issue.fields.status.name + endRow;			
 
-			tempJiraSummary = tempJiraSummary + "Created By: ";
-			tempJiraSummary = tempJiraSummary + issue.fields.creator.displayName + endRow;
+				tempJiraSummary = tempJiraSummary + "Created By: ";
+				tempJiraSummary = tempJiraSummary + issue.fields.creator.displayName + endRow;
 
-			tempJiraSummary = tempJiraSummary + "Assigned To: ";
-			tempJiraSummary = tempJiraSummary + issue.fields.assignee.displayName + endRow;
+				tempJiraSummary = tempJiraSummary + "Assigned To: ";
+				tempJiraSummary = tempJiraSummary + issue.fields.assignee.displayName + endRow;
 
-			tempJiraSummary = tempJiraSummary + "Create Date: ";
-			tempJiraSummary = tempJiraSummary + issue.fields.created + endRow + endRow;
+				tempJiraSummary = tempJiraSummary + "Create Date: ";
+				tempJiraSummary = tempJiraSummary + issue.fields.created + endRow;
 			
-			tempJiraSummary = tempJiraSummary + "Last Update Date: ";
-			tempJiraSummary = tempJiraSummary + issue.fields.updated + endRow;
+				tempJiraSummary = tempJiraSummary + "Last Update Date: ";
+				tempJiraSummary = tempJiraSummary + issue.fields.updated + endRow;
 
-			tempJiraSummary = tempJiraSummary + "Link: ";
-			tempJiraSummary = tempJiraSummary + jiraUrl + issue.key + endRow + endRow;
+				tempJiraSummary = tempJiraSummary + "Link: ";
+				tempJiraSummary = tempJiraSummary + jiraUrl + issue.key + endRow + endRow;
 
-			body = body + tempJiraSummary;
+				body = body + tempJiraSummary;
+			}
 		}
 
         mail to: "filip.ludma@exaxe.com", 
