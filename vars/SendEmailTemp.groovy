a6ee879580124ad2bf0f627f27559fcb9e4e2fed
ExaxeDevelopment <build.development@exaxe.com>
2017-10-12 14:40:55 +0100

    - Updated SendEmailTemp.groorvy

--- vars/SendEmailTemp.groovy
+++ vars/SendEmailTemp.groovy
@@ -1,23 +1,44 @@
 import hudson.Util;
 
+def selectedJiraRelease = "";
+def selectedJiraProjectKey = "";
+
 node{
 
     stage("Success Notification"){
         
-		// EXECUTE
-		List<String> issueKeys = jiraSearch(jql: "project = EPOS AND status in (Resolved)")	
+		// INPUT PARAMS
+		selectedJiraRelease = SelectedJiraRelease;
+		selectedJiraProjectKey = SelectedJiraProjectKey; 
 
-		def temp  = "${env.BUILD_URL}";
-		def jiraUrl = "https://exaxejira.atlassian.net/browse/"
-		if(issueKeys != null){
-			for(String key : issueKeys){
-				temp = temp + jiraUrl + key + "\r\n";
-			}
+		// JIRAT TICKETS
+		def devJiraTickets;
+		def resolvedJiraTickets;
+
+		wrap([$class: 'hudson.plugins.jira.JiraCreateReleaseNotes', jiraProjectKey: selectedJiraProjectKey,
+        jiraRelease: selectedJiraRelease, jiraEnvironmentVariable: 'notes', jiraFilter: 'status in (Open, Review, "To Do")']) 
+		{
+			println "=============================================="
+			println "GET JIRA TICKETS IN DEV"
+			devJiraTickets =  env.notes
+			println "=============================================="
 		}
+    
+        wrap([$class: 'hudson.plugins.jira.JiraCreateReleaseNotes', jiraProjectKey: 'ALEUAT',
+        jiraRelease: 'Next 1.2.X Release', jiraEnvironmentVariable: 'notes', jiraFilter: 'status in (Resolved")']) 
+		{
+	    
+			println "=============================================="
+			println "GET RESOLVED JIRA TICKETS TO TEST"
+			resolvedJiraTickets = env.notes
+			println "=============================================="
+	    }
+
+		def temp  = "${env.BUILD_URL}" + "\r\n";
 
         mail to: "filip.ludma@exaxe.com", 
         subject: " ${JOB_NAME} (Build ${currentBuild.displayName} / ${currentBuild.result})", 
-		body: temp 
+		body: temp + devJiraTickets + "\r\n" + "\r\n" + resolvedJiraTickets; 
     }
 }
 
