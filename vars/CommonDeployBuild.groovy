b1e772f29af03d88ef11913eb094ad38e7440e80
ExaxeDevelopment <build.development@exaxe.com>
2017-10-13 10:26:09 +0100

    - Updated CommonDeployBuild groovy;

--- vars/CommonDeployBuild.groovy
+++ vars/CommonDeployBuild.groovy
@@ -14,6 +14,9 @@ def deployConfigurationAction = "#";
 
 def duration = "";
 
+def endRow = "\r\n";
+def selectedJiraProjectKey = "";
+
 try{
     node{
 
@@ -71,9 +74,61 @@ try{
             stage("Success Notification"){
                 echo duration;
                 
+				def body = "${env.BUILD_URL} \r\n ${duration}" 
+
+				envVars = env.getEnvironment()
+				if(envVars.containsKey("SelectedJiraProjectKey")){
+					
+					// INPUT PARAM
+					selectedJiraProjectKey = SelectedJiraProjectKey; 
+
+					def jiraStatuses = "";
+					switch (selectedJiraProjectKey) {
+						case "OF":
+							jiraStatuses = "status in ('Dev Complete', 'Test In Progress')";
+							selectedJiraProjectKey = "'OF'";
+							break
+						default:
+							jiraStatuses = "status = Resolved";
+							break
+					}
+
+					def jql = "project = " + selectedJiraProjectKey + " AND " + jiraStatuses;
+					def issues = jiraJqlSearch jql: jql, site: 'exaxejira', failOnError: true;
+		
+					body = "${env.BUILD_URL} \r\n ${duration}" + "\r\n\r\n" + "JIRA RELEASE NOTES" + "\r\n\r\n";
+					def jiraUrl = "https://exaxejira.atlassian.net/browse/"
+
+					for(def issue in issues.data.issues){
+						def tempJiraSummary = "";   
+						tempJiraSummary = "Summary: ";
+						tempJiraSummary = tempJiraSummary + issue.key + ": " + issue.fields.summary + endRow;
+
+						tempJiraSummary = tempJiraSummary + "Status: ";
+						tempJiraSummary = tempJiraSummary +  issue.fields.status.name + endRow;			
+
+						tempJiraSummary = tempJiraSummary + "Created By: ";
+						tempJiraSummary = tempJiraSummary + issue.fields.creator.displayName + endRow;
+
+						tempJiraSummary = tempJiraSummary + "Assigned To: ";
+						tempJiraSummary = tempJiraSummary + issue.fields.assignee.displayName + endRow;
+
+						tempJiraSummary = tempJiraSummary + "Create Date: ";
+						tempJiraSummary = tempJiraSummary + issue.fields.created + endRow;
+			
+						tempJiraSummary = tempJiraSummary + "Last Update Date: ";
+						tempJiraSummary = tempJiraSummary + issue.fields.updated + endRow;
+
+						tempJiraSummary = tempJiraSummary + "Link: ";
+						tempJiraSummary = tempJiraSummary + jiraUrl + issue.key + endRow + endRow;
+
+						body = body + tempJiraSummary;
+					}
+				}
+
                 mail to: "${DEV_TEAM_EMAIL},${QA_TEAM_EMAIL},${BA_TEAM_EMAIL},${PM_TEAM_EMAIL}", 
                 subject: " ${JOB_NAME} (Build ${currentBuild.displayName} / ${currentBuild.result})", 
-                body: "${env.BUILD_URL} \r\n ${duration}" 
+                body: body;
             }
         }
         catch(err){
