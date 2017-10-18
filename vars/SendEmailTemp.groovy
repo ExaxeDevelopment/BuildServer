dc12e7605e87d1a42f9108c24623ad48f7bc0d7c
ExaxeDevelopment <build.development@exaxe.com>
2017-10-18 10:52:00 +0100

    - Updated Send Email temp to load groovy class;

--- vars/SendEmailTemp.groovy
+++ vars/SendEmailTemp.groovy
@@ -1,4 +1,7 @@
 import hudson.Util;
+import org.thoughtslive.jenkins.plugins.jira.api.Component;
+import org.thoughtslive.jenkins.plugins.jira.api.Version;
+import org.thoughtslive.jenkins.plugins.jira.Site;
 
 def endRow = "\r\n";
 def selectedJiraRelease = "";
@@ -14,48 +17,11 @@ node{
 			// INPUT PARAMS
 			selectedJiraProjectKey = SelectedJiraProjectKey; 
 
-			def jiraStatuses = "";
-			switch (selectedJiraProjectKey) {
-				case "OF":
-					jiraStatuses = "status in ('Dev Complete', 'Test In Progress')";
-					selectedJiraProjectKey = "'OF'";
-					break
-				default:
-					jiraStatuses = "status = Resolved";
-					break
-			}
-
-			def jql = "project = " + selectedJiraProjectKey + " AND " + jiraStatuses;
-			def issues = jiraJqlSearch jql: jql, site: 'exaxejira', failOnError: true;
-		
-			body = "${env.BUILD_URL}" + "\r\n\r\n" + "JIRA RELEASE NOTES" + "\r\n\r\n";
-			def jiraUrl = "https://exaxejira.atlassian.net/browse/"
-
-			for(def issue in issues.data.issues){
-				def tempJiraSummary = "";   
-				tempJiraSummary = "Summary: ";
-				tempJiraSummary = tempJiraSummary + issue.key + ": " + issue.fields.summary + endRow;
-
-				tempJiraSummary = tempJiraSummary + "Status: ";
-				tempJiraSummary = tempJiraSummary +  issue.fields.status.name + endRow;			
-
-				tempJiraSummary = tempJiraSummary + "Created By: ";
-				tempJiraSummary = tempJiraSummary + issue.fields.creator.displayName + endRow;
-
-				tempJiraSummary = tempJiraSummary + "Assigned To: ";
-				tempJiraSummary = tempJiraSummary + issue.fields.assignee.displayName + endRow;
-
-				tempJiraSummary = tempJiraSummary + "Create Date: ";
-				tempJiraSummary = tempJiraSummary + issue.fields.created + endRow;
-			
-				tempJiraSummary = tempJiraSummary + "Last Update Date: ";
-				tempJiraSummary = tempJiraSummary + issue.fields.updated + endRow;
-
-				tempJiraSummary = tempJiraSummary + "Link: ";
-				tempJiraSummary = tempJiraSummary + jiraUrl + issue.key + endRow + endRow;
-
-				body = body + tempJiraSummary;
-			}
+			// PREPARE CLASS OBJECT
+			def jiraReleaseNotificationEmailClass = new jiraReleaseNotificationEmail();
+	    
+	    	// START EXECUTION
+			body = jiraReleaseNotificationEmailClass.createJiraReleaseNotes(selectedJiraProjectKey)
 		}
 
         mail to: "filip.ludma@exaxe.com", 
