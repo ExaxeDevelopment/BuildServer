8a2e90897bb40abfa9f70150924920fb11be0457
ExaxeDevelopment <build.development@exaxe.com>
2017-10-23 14:15:57 +0100

    - Updated jiraReleaseNotificationEmail;

--- vars/jiraReleaseNotificationEmail.groovy
+++ vars/jiraReleaseNotificationEmail.groovy
@@ -7,8 +7,8 @@ class jiraReleaseNotificationEmail implements Serializable {
 		def jiraUrl = "https://exaxejira.atlassian.net/browse/"
 
 		// read the file from path
-		def template = new releaseJiraEmailTemplate()
-		println template.getReleaseNotesTemplate();
+		String fileContents = new File('temTemplate.html').getText('UTF-8')
+		println fileContents;
 		
 		for(def issue in issues.data.issues){
 			def tempJiraSummary = "";   
