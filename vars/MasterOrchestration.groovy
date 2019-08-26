c6fe9067541956ca760fb7f3f70400ee0ee515dd
Ricardo Bicho <Ricardo.Bicho@exaxe.com>
2019-08-26 17:14:45 +0100

    - Added getIntermediateDevelopmentSteps() to MasterOrchestration;

--- vars/MasterOrchestration.groovy
+++ vars/MasterOrchestration.groovy
@@ -26,6 +26,10 @@ try{
 					allSteps = getDevelopmentSteps(buildFromStage, buildToStage);
 					break
 
+				case "IntermediateDevelopment":
+					allSteps = getIntermediateDevelopmentSteps(buildFromStage, buildToStage);
+					break
+
 				case "Integration":
 					allSteps = getIntegrationSteps(buildFromStage, buildToStage);
 					break
@@ -206,6 +210,45 @@ def getDevelopmentSteps(buildFromStage, buildToStage){
 	////Map<String,String> map10 = new HashMap<String,String>();
 	////map10.put("MasterBuild-Orchestration-Integration", BASE_BUILD_SERVER)
 	////steps.put("Integration", map10);
+
+	//// filter the steps/stages
+	Map<String,Map<String,String>> finalSteps = getFilteredSteps(steps, buildFromStage, buildToStage);
+
+	finalSteps;	
+}
+
+/// Method that returns the development steps for the Intermediate Build
+def getIntermediateDevelopmentSteps(buildFromStage, buildToStage){
+	Map<String,Map<String,String>> steps = new LinkedHashMap<String,Map<String,String>>();
+
+	Map<String,String> map02 = new HashMap<String,String>();
+	map02.put("Intermediate-MasterBuild-AdminPlus-Dev002", ADMIN_PLUS_BUILD_SERVER)
+	map02.put("Intermediate-MasterBuild-AdvicePlus-Dev002", ADVICE_PLUS_BUILD_SERVER)
+	map02.put("Intermediate-MasterBuild-ChannelPlus-Dev002", DISTRIBUTION_PLUS_BUILD_SERVER)
+	map02.put("Intermediate-MasterBuild-PolicyEBI-Dev001", BASE_BUILD_SERVER)
+	steps.put("Backend Services", map02);
+	  
+	Map<String,String> map03 = new HashMap<String,String>();
+	map03.put("Intermediate-MasterBuild-Apex-Dev002", APEX_BUILD_SERVER)	
+	steps.put("APEX", map03);
+	 
+	Map<String,String> map04 = new HashMap<String,String>();
+	map04.put("Intermediate-MasterBuild-WebServices-Dev002", API_BUILD_SERVER)	
+	steps.put("Web Services", map04);
+
+	Map<String,String> map05 = new HashMap<String,String>();
+	map05.put("Build-SystemConfigurationUI-Dev002", BASE_BUILD_SERVER)	
+	map05.put("Build-AdminPlusUI-Dev002", ADMIN_PLUS_BUILD_SERVER)
+	map05.put("Build-PointOfSaleUI-Dev002", ADVICE_PLUS_BUILD_SERVER)	
+	map05.put("Build-ChannelPlusUI-Dev002", DISTRIBUTION_PLUS_BUILD_SERVER)
+	map05.put("Build-Exaxe.Portals-Dev002", API_BUILD_SERVER)
+	map05.put("Build-Hansard.Portals-Dev002", API_BUILD_SERVER)	
+	steps.put("Web Sites", map05);
+
+	Map<String,String> map06 = new HashMap<String,String>();
+	map06.put("Build-DatabaseReconciliation-All", APEX_BUILD_SERVER)
+	steps.put("Database Reconciliation", map06);
+
 
 	//// filter the steps/stages
 	Map<String,Map<String,String>> finalSteps = getFilteredSteps(steps, buildFromStage, buildToStage);
