b2120074a62ed209c6801f68939fd0eb3d8a8261
Ian Thompson <Ian.Thompson@exaxehub.onmicrosoft.com>
2020-09-01 08:44:23 +0100

    do it properly

--- vars/masterBuildStepsByProject.groovy
+++ vars/masterBuildStepsByProject.groovy
@@ -372,36 +372,36 @@ class masterBuildStepsByProject implements Serializable {
 	private List<String> getMasterBuildReleaseVXX_X(String version)
 	{
 		List<String> buildItems = new ArrayList<String>()
-		buildItems.add("Build-Exaxe.Types-Release-V".version); 
-		buildItems.add("Build-Exaxe.Libraries-Release-V".version); 
-		buildItems.add("Build-Exaxe.Rest-Release-V".version); 
-		buildItems.add("Build-Exaxe.Web-Release-V".version); 
-		buildItems.add("Build-Exaxe.Common-Release-V".version); 
-		buildItems.add("Build-Exaxe.External-Release-V".version); 
-		buildItems.add("Build-Exaxe.Agent-Release-V".version); 
-		buildItems.add("Build-Exaxe.App-Release-V".version); 
-		buildItems.add("Build-Exaxe.Configuration-Release-V".version); 
-		buildItems.add("Build-Exaxe.Rules-Release-V".version); 
-		buildItems.add("Build-Exaxe.Comment-Release-V".version); 
-		buildItems.add("Build-Exaxe.Document-Release-V".version); 
-		buildItems.add("Build-Exaxe.SystemConfiguration-Release-V".version); 
-		buildItems.add("Build-Exaxe.Users-Release-V".version);
-		buildItems.add("Build-Exaxe.Accounting-Release-V".version);
-		buildItems.add("Build-Exaxe.Auth-Release-V".version);
-		buildItems.add("Build-Exaxe.UIElements-Release-V".version);
-		buildItems.add("Build-SystemConfigurationUI-Release-V".version);
-		buildItems.add("Build-Exaxe.Calculations-Release-V".version);
-		buildItems.add("Build-Exaxe.Client-Release-V".version);
-		buildItems.add("Build-Exaxe.ChannelPlus-Release-V".version); 
-		buildItems.add("Build-Exaxe.AdminPlus-Release-V".version); 
-		buildItems.add("Build-Exaxe.Apex-Release-V".version); 
-		buildItems.add("Build-Exaxe.Aggregate-Release-V".version); 
-		buildItems.add("Build-Exaxe.WebApi-Release-V".version); 
-		buildItems.add("Build-Exaxe.Apex.Workflow-Release-V".version); 
-		buildItems.add("Build-Exaxe.Apex.Activities-Release-V".version); 
-		buildItems.add("Build-Exaxe.Apex.Server-Release-V".version); 
-		buildItems.add("Build-AdminPlusUI-Release-V".version); 
-		buildItems.add("Build-ChannelPlusUI-Release-V".version); 	
+		buildItems.add("Build-Exaxe.Types-Release-V"+version); 
+		buildItems.add("Build-Exaxe.Libraries-Release-V"+version); 
+		buildItems.add("Build-Exaxe.Rest-Release-V"+version); 
+		buildItems.add("Build-Exaxe.Web-Release-V"+version); 
+		buildItems.add("Build-Exaxe.Common-Release-V"+version); 
+		buildItems.add("Build-Exaxe.External-Release-V"+version); 
+		buildItems.add("Build-Exaxe.Agent-Release-V"+version); 
+		buildItems.add("Build-Exaxe.App-Release-V"+version); 
+		buildItems.add("Build-Exaxe.Configuration-Release-V"+version); 
+		buildItems.add("Build-Exaxe.Rules-Release-V"+version); 
+		buildItems.add("Build-Exaxe.Comment-Release-V"+version); 
+		buildItems.add("Build-Exaxe.Document-Release-V"+version); 
+		buildItems.add("Build-Exaxe.SystemConfiguration-Release-V"+version); 
+		buildItems.add("Build-Exaxe.Users-Release-V"+version);
+		buildItems.add("Build-Exaxe.Accounting-Release-V"+version);
+		buildItems.add("Build-Exaxe.Auth-Release-V"+version);
+		buildItems.add("Build-Exaxe.UIElements-Release-V"+version);
+		buildItems.add("Build-SystemConfigurationUI-Release-V"+version);
+		buildItems.add("Build-Exaxe.Calculations-Release-V"+version);
+		buildItems.add("Build-Exaxe.Client-Release-V"+version);
+		buildItems.add("Build-Exaxe.ChannelPlus-Release-V"+version); 
+		buildItems.add("Build-Exaxe.AdminPlus-Release-V"+version); 
+		buildItems.add("Build-Exaxe.Apex-Release-V"+version); 
+		buildItems.add("Build-Exaxe.Aggregate-Release-V"+version); 
+		buildItems.add("Build-Exaxe.WebApi-Release-V"+version); 
+		buildItems.add("Build-Exaxe.Apex.Workflow-Release-V"+version); 
+		buildItems.add("Build-Exaxe.Apex.Activities-Release-V"+version); 
+		buildItems.add("Build-Exaxe.Apex.Server-Release-V"+version); 
+		buildItems.add("Build-AdminPlusUI-Release-V"+version); 
+		buildItems.add("Build-ChannelPlusUI-Release-V"+version); 	
 
 		return buildItems;		
 	}
