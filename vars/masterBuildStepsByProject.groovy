5ffd1731ba179e59f5c1e1052b770d12dbccd552
Ian Thompson <build.development@majesco.com>
2021-05-27 14:21:39 +0100

    reorder build

--- vars/masterBuildStepsByProject.groovy
+++ vars/masterBuildStepsByProject.groovy
@@ -483,6 +483,7 @@ class masterBuildStepsByProject implements Serializable {
 		buildItems.add("Build-Exaxe.Rest-Release-V"+version); 
 		buildItems.add("Build-Exaxe.Web-Release-V"+version); 
 		buildItems.add("Build-Exaxe.Common-Release-V"+version); 
+		buildItems.add("Build-Exaxe.Auth-Release-V"+version);
 		buildItems.add("Build-Exaxe.External-Release-V"+version); 
 		buildItems.add("Build-Exaxe.Agent-Release-V"+version); 
 		buildItems.add("Build-Exaxe.App-Release-V"+version); 
@@ -493,7 +494,6 @@ class masterBuildStepsByProject implements Serializable {
 		buildItems.add("Build-Exaxe.SystemConfiguration-Release-V"+version); 
 		buildItems.add("Build-Exaxe.Users-Release-V"+version);
 		buildItems.add("Build-Exaxe.Accounting-Release-V"+version);
-		buildItems.add("Build-Exaxe.Auth-Release-V"+version);
 		buildItems.add("Build-Exaxe.UIElements-Release-V"+version);
 		buildItems.add("Build-Exaxe.Calculations-Release-V"+version);
 		buildItems.add("Build-Exaxe.CalculationManagers-Release-V"+version);
