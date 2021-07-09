43d9403c4e32894dc735e22e6747d0937f95d4c0
Ian Thompson <build.development@majesco.com>
2021-07-09 13:17:08 +0100

    add integration

--- vars/MasterOrchestration.groovy
+++ vars/MasterOrchestration.groovy
@@ -152,10 +152,10 @@ def getDevelopmentSteps(buildFromStage, buildToStage){
 	map01.put("MasterBuild-Base-Dev001", BASE_BUILD_SERVER)
 	steps.put("Base/Common", map01);
 
-	//// Temporary stage to allow integration to be kicked off automatically
-	//// Map<String,String> mapIntegration = new HashMap<String,String>();
-	//// mapIntegration.put("MasterBuild-Orchestration-Integration", BASE_BUILD_SERVER)
-	//// steps.put("Integration", mapIntegration);  
+	Temporary stage to allow integration to be kicked off automatically
+	Map<String,String> mapIntegration = new HashMap<String,String>();
+	mapIntegration.put("MasterBuild-Orchestration-Integration", BASE_BUILD_SERVER)
+	steps.put("Integration", mapIntegration);  
 		 
 	Map<String,String> map02 = new HashMap<String,String>();
 	map02.put("MasterBuild-AdminPlus-Dev002", ADMIN_PLUS_BUILD_SERVER)
