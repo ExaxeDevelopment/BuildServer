89611ba88c2751a30b42069b4feda9de9ab13013
Ian Thompson <build.development@majesco.com>
2021-08-08 16:59:39 +0100

    correct integration

--- vars/MasterOrchestration.groovy
+++ vars/MasterOrchestration.groovy
@@ -280,7 +280,7 @@ def getIntegrationSteps(buildFromStage, buildToStage){
 	steps.put("Backend Services", map02);
 	  
 	Map<String,String> map03 = new HashMap<String,String>();
-	map03.put("Build-Exaxe.Apex.Workflow-Dev002", APEX_BUILD_SERVER)	
+	map03.put("Build-Exaxe.Apex.Workflow-Dev001", APEX_BUILD_SERVER)	
 	steps.put("APEX Workflow", map03);
 	 
 	Map<String,String> map04 = new HashMap<String,String>();
