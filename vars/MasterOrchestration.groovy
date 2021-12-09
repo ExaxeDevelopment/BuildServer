20752d1ce9cc65b2ccc4e7c4d83fdf9a2e45e584
Ian Thompson <build.development@majesco.com>
2021-12-09 16:08:33 +0000

    add db orchestration revert intermediate

--- vars/MasterOrchestration.groovy
+++ vars/MasterOrchestration.groovy
@@ -26,6 +26,10 @@ try{
 					allSteps = getDevelopmentSteps(buildFromStage, buildToStage);
 					break
 
+				case "DatabaseReconciliation":
+					allSteps = getDatabaseReconciliationSteps(buildFromStage, buildToStage);
+
+					break
 				case "IntermediateDevelopment":
 					allSteps = getIntermediateDevelopmentSteps(buildFromStage, buildToStage);
 					break
@@ -266,25 +270,40 @@ def getIntermediateDevelopmentSteps(buildFromStage, buildToStage){
 	map02.put("Intermediate-MasterBuild-AdminPlus-Dev002", ADMIN_PLUS_BUILD_SERVER)
 	map02.put("Intermediate-MasterBuild-AdvicePlus-Dev002", ADVICE_PLUS_BUILD_SERVER)
 	map02.put("Intermediate-MasterBuild-ChannelPlus-Dev002", DISTRIBUTION_PLUS_BUILD_SERVER)
-	////steps.put("Backend Services", map02);
+	steps.put("Backend Services", map02);
 	  
 	Map<String,String> map03 = new HashMap<String,String>();
 	map03.put("Intermediate-MasterBuild-Apex-Dev002", APEX_BUILD_SERVER)	
-	////steps.put("APEX", map03);
+	steps.put("APEX", map03);
 	 
 	Map<String,String> map04 = new HashMap<String,String>();
 	map04.put("Intermediate-MasterBuild-WebServices-Dev002", API_BUILD_SERVER)	
-	////steps.put("Web Services", map04);
+	steps.put("Web Services", map04);
 
 	Map<String,String> map05 = new HashMap<String,String>();
 	map05.put("Build-SystemConfigurationUI-Dev002", BASE_BUILD_SERVER)	
 	map05.put("Build-AdminPlusUI-Dev002", ADMIN_PLUS_BUILD_SERVER)
 	map05.put("Build-PointOfSaleUI-Dev002", ADVICE_PLUS_BUILD_SERVER)	
 	map05.put("Build-ChannelPlusUI-Dev002", DISTRIBUTION_PLUS_BUILD_SERVER)
-	//// map05.put("Build-Exaxe.Portals-Dev002", API_BUILD_SERVER)
+	////map05.put("Build-Exaxe.Portals-Dev002", API_BUILD_SERVER)
 	map05.put("Build-Hansard.Portals-Dev002", API_BUILD_SERVER)	
-	////steps.put("Web Sites", map05);
+	steps.put("Web Sites", map05);
 
+	Map<String,String> map06 = new HashMap<String,String>();
+	map06.put("Build-DatabaseReconciliation-All", APEX_BUILD_SERVER)
+	steps.put("Database Reconciliation", map06);
+
+	//// filter the steps/stages
+	Map<String,Map<String,String>> finalSteps = getFilteredSteps(steps, buildFromStage, buildToStage);
+
+	finalSteps;	
+}
+
+/// Method that returns the development steps for the Intermediate Build
+def getDatabaseReconciliationSteps(buildFromStage, buildToStage){
+	Map<String,Map<String,String>> steps = new LinkedHashMap<String,Map<String,String>>();
+
+	Map<String,String> map02 = new HashMap<String,String>();
 	Map<String,String> map06 = new HashMap<String,String>();
 	map06.put("Build-DatabaseReconciliation-All", APEX_BUILD_SERVER)
 	steps.put("Database Reconciliation", map06);
