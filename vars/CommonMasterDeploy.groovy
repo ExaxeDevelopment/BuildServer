4780dd5bdbebb5cb46331efb120dbf1e5b552f9f
Ian Thompson <build.development@majesco.com>
2022-05-11 14:13:49 +0100

    fix

--- vars/CommonMasterDeploy.groovy
+++ vars/CommonMasterDeploy.groovy
@@ -67,7 +67,7 @@ try{
 				
                 mail to: "${DEV_TEAM_EMAIL}", 
                 subject: " ${JOB_NAME} (Build ${currentBuild.displayName} / ${currentBuild.result})", 
-                body: "<html><body>${css}<div class='good banner'>&nbsp;</div><a href='${env.BUILD_URL}'>${JOB_NAME} ${currentBuild.displayName} / ${currentBuild.result}</a><br/> ${duration} <br/> Build Type: ${buildTypeAction}<div>${embeddedImage}</div></body></html>",
+                body: "<html><body>${css}<div class='good banner'>&nbsp;</div><a href='${env.BUILD_URL}'>${JOB_NAME} ${currentBuild.displayName} / ${currentBuild.result}</a><br/> ${duration} <div>${embeddedImage}</div></body></html>",
 				mimeType: 'text/html'
             }
         }
@@ -87,7 +87,7 @@ catch(err){
         stage("Error Notification"){
             mail to: "${DEV_TEAM_EMAIL}", 
             subject: " ${JOB_NAME} (Build ${currentBuild.displayName} / ${currentBuild.result})", 
-            body:"<html><body>${css}<div class='bad banner'>&nbsp;</div><a href='${env.BUILD_URL}'>${JOB_NAME} ${currentBuild.displayName} / ${currentBuild.result}</a><br/> ${duration} <br/> Build Type: ${buildTypeAction}<div>${embeddedImage}</div></body></html>",
+            body:"<html><body>${css}<div class='bad banner'>&nbsp;</div><a href='${env.BUILD_URL}'>${JOB_NAME} ${currentBuild.displayName} / ${currentBuild.result}</a><br/> ${duration} <div>${embeddedImage}</div></body></html>",
 				mimeType: 'text/html'
         }
     }	
