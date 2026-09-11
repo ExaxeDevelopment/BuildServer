e9036f904b9872f474939e6ead9f9fd26cc4d6c6
Ian Thompson <Ian87205@majesco.com>
2026-09-11 09:41:34 +0100

    fix remote trigger

--- vars/MasterOrchestration.groovy
+++ vars/MasterOrchestration.groovy
@@ -216,7 +216,15 @@ def getRemoteJobRequest(serverName, job, token, mapStatuses, css, embeddedImage)
 		try{
 			stage("${job}"){
 				echo "Trigering job: ${job}"
-				def handle = triggerRemoteJob(remoteJenkinsName: serverName, job: job, token: token, pollInterval: 30, blockBuildUntilComplete: true);
+				def handle = triggerRemoteJob(remoteJenkinsName: serverName,
+					job: job, 
+					auth: TokenAuth(
+						apiToken: Secret.fromString(token), 
+						userName: "admin"
+					), 					
+					////token: token, 
+					pollInterval: 30, 
+					blockBuildUntilComplete: true);
 				def status = handle.getBuildStatus();
 				echo "Remote status from ${job}: ${status.toString()}";
 				def result = handle.getBuildResult();
